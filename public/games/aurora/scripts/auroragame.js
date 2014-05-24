dde608ec6719cd5e9b0ee66cac9d8ebfa5560785
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2014-05-24 17:04:53 +0200

    Player disconnection handling, deleting sprites on screen

--- public/games/aurora/scripts/auroragame.js
+++ public/games/aurora/scripts/auroragame.js
@@ -28,7 +28,6 @@ requirejs([
 
     // listeners
     server.addEventListener('playerconnect', function (netPlayer) {
-        console.log('player connected');
         playerManager.add(netPlayer);
     });
 
@@ -37,25 +36,10 @@ requirejs([
     function preload () {
         game.load.image('red', 'assets/img/red.png');
         game.load.image('blue', 'assets/img/blue.png');
-        console.log('preload');
-        console.log(game.world);
     }
 
     function create () {
-        red = game.add.sprite(game.world.centerX, game.world.centerY, 'red');
-        red.anchor.setTo(0.5, 0.5);
-        red.alpha = 0;
 
-        game.stage.backgroundColor = 0x2d2d2d;
-
-        var tween = game.add.tween(red).to({ x: 600 }, 2000, Phaser.Easing.Linear.None)
-        .to({ y: 300 }, 1000, Phaser.Easing.Linear.None)
-        .to({ x: 100 }, 2000, Phaser.Easing.Linear.None)
-        .to({ y: 100 }, 1000, Phaser.Easing.Linear.None)
-        .loop()
-        .start();
-
-        game.add.tween(red).to( { alpha: 1 }, 2000, Phaser.Easing.Linear.None, true, 0, 1000, true);
     }
 
     function update () {
--- public/games/aurora/scripts/playerManager.js
+++ public/games/aurora/scripts/playerManager.js
@@ -11,26 +11,57 @@ define(function () {
     }
 
     PlayerManager.prototype.add = function(netPlayer) {
-    	var self = this;
-    	var sprite = game.add.sprite(game.rnd.integerInRange(0, game.world.centerX), game.rnd.integerInRange(0, game.world.centerY), 'red');
-    	this.players[netPlayer.id] = {
-	    	'sprite': sprite
-    	};
+    	var self = this,
+    		id = netPlayer.id;
+
+    	this.queue.push(netPlayer);
+
+        console.log('player connected');
 
     	netPlayer.addEventListener('disconnect', function () {
+    		netPlayer.removeAllListeners();
+    		var player = self.players[id];
+    		player.left = true;
+    		if (self.players[player.pair].left) {
+    			console.log('REMOVING PLAYERS ' + player.id + ' AND ' + player.pair);
+    			// remove the 2 sprites
+    			player.sprite.kill();
+    			self.players[player.pair].sprite.kill();
+    			delete self.players[player.id];
+    			delete self.players[player.pair];
+    		}
 	        console.log('player disconnected');
-	        sprite.kill();
-	        delete self.players[netPlayer.id];
+	        console.log(player);
+	        console.log('players');
+	        console.log(self.players);
 	    });
 
 	    netPlayer.addEventListener('move', function (data) {
-	    	console.log(data);
+	    	console.log('move: ' + JSON.stringify(data));
 	    });
 
 		netPlayer.addEventListener('stop', function (data) {
-	    	console.log(data);
+	    	console.log('stop: ' + JSON.stringify(data));
 	    });
 
+    	if (this.queue.length < 2) {
+    		return;
+    	}
+
+    	var p1 = this.queue.shift(),
+    		p2 = this.queue.shift();
+
+    	this.players[p1.id] = {
+	    	'sprite': game.add.sprite(game.rnd.integerInRange(0, game.world.centerX), game.rnd.integerInRange(0, game.world.centerY), 'red'),
+	    	'pair': p2.id
+    	};
+
+    	this.players[p2.id] = {
+	    	'sprite': game.add.sprite(game.rnd.integerInRange(0, game.world.centerX), game.rnd.integerInRange(0, game.world.centerY), 'blue'),
+	    	'pair': p1.id
+    	};
+
+    	console.log(this.players);
     };
 
     return PlayerManager;
